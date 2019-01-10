e6bc6743fc17207689408138b024f7fd0c37655f
Safrin13 <42463501+Safrin13@users.noreply.github.com>
2019-01-11 01:02:51 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function myTime(){
+      document.getElementById('time').innerHTML = Date()
+     };
+function myImageOn(){
+      document.getElementById('myImage').src='image/on.gif'
+         };
+function myImageOff(){
+      document.getElementById('myImage').src='image/off.gif'
+         };
+window.alert('welcome');
+function myParagraph(){
+      document.getElementById('demo').innerHTML = 'changed paragraph'
+     };
